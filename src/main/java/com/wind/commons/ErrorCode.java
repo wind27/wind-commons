07c7cddd95a2bb20d2ba6d81ad9088ac0b6a500a
qianchun <qianchun@huli.com>
2018-08-06 18:56:15 +0800

    init

---
+++ src/main/java/com/wind/commons/ErrorCode.java
@@ -0,0 +1,44 @@
+package com.wind.commons;
+
+/**
+ * ErrorCode
+ *
+ * @author qianchun 17/7/13
+ **/
+public enum ErrorCode {
+
+    SUCCESS(            0,  "success"),
+    FAIL(               1,  "fail"),
+    SYS_ERROR(          2,  "sys_error"),
+    PARAM_ERROR(        3,  "param_error"),
+    NO_PREMISSION(      4,  "no_premission"),
+    NOT_EXIST(          5,  "not_exist"),
+    DISABLED(           6,  "disabled"),
+    ERROR(              -1,  "error");
+
+
+
+    private Integer value;
+    private String name;
+
+    ErrorCode(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
