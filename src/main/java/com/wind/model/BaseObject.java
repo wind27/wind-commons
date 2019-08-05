5b4bd812b14b3702a94d38293213394c8958df66
qianchun <qianchun@huli.com>
2019-08-05 11:25:26 +0800

    test

---
+++ src/main/java/com/wind/model/BaseObject.java
@@ -0,0 +1,30 @@
+package com.wind.model;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * BaseObject
+ *
+ * @author qianchun
+ * @date 2019/7/29
+ **/
+public class BaseObject {
+    private static final long serialVersionUID = 2744190391613295958L;
+
+    public BaseObject() {
+    }
+
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    public boolean equals(Object object) {
+        return EqualsBuilder.reflectionEquals(this, object, new String[0]);
+    }
+
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, new String[0]);
+    }
+}
